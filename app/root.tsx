6ee81cca5fe03a392f2ec2d834020f4f468bc35b
Sergio Diosdado <sgdc_98@hotmail.com>
2024-02-04 11:52:13 -0600

    Add tailwind and prettier

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import { cssBundleHref } from '@remix-run/css-bundle'
+import type { LinksFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
@@ -7,18 +7,20 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
+import stylesheet from '~/tailwind.css'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+  { rel: 'stylesheet', href: stylesheet },
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+]
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
@@ -29,5 +31,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,33 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Boda Jacqueline y Andrés" },
+    { name: "description", content: "Invitación boda Jacqueline y Andrés" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <main className="flex min-h-svh flex-col items-center justify-between bg-green-50 text-gray-500">
+        <img
+          src="./images/logo.png"
+          alt="Logo boda Jacqueline y Andrés"
+          className="w-16"
+        />
+        <section className="flex h-full flex-1 flex-col items-center justify-center">
+          <p className="text-xs">Jacqueline & Andrés</p>
+          <div className="flex flex-col gap-4 text-center text-5xl font-thin">
+            <p>
+              <span className="font-extralight italic">Our love</span> is a
+            </p>
+            <p>journey that</p>
+            <p>begins with</p>
+            <p>forever</p>
+          </div>
+          <p className="text-xs">04.05.2024</p>
+        </section>
+      </main>
+    </>
   );
 }
